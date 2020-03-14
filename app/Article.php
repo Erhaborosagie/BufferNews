8b8a6df5926e05d549bfb1b14b42865866ee4a1f
Osagie Erhabor <erhaborosagie@gmail.com>
2020-03-14 23:37:18 +0100

    Created ArticleController

--- app/Article.php
+++ app/Article.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $fillable = ['title', 'body'];
+    protected $fillable = ['title', 'body', 'user_id'];
 
     public function user(){
         return $this->belongsTo(User::class);
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{article}', 'ArticlesController@show');
