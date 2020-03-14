01dd670f231812b1e37a79d60894d20797f241bd
Osagie Erhabor <erhaborosagie@gmail.com>
2020-03-15 00:51:59 +0100

    Created seeder for article and user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('adminPass'),
+        ]);
+        User::create([
+            'name' => 'Author',
+            'email' => 'author@example.com',
+            'password' => Hash::make('authorPass'),
+        ]);
+        User::create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('userPass'),
+        ]);
+
+    }
+}
